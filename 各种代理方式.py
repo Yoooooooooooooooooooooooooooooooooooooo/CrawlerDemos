5507d7602f1a9cabb962ae7811d1875e8885b5ef
Yoooooooooooooooooooooooooooooooooooooo <348883824@qq.com>
2018-12-05 17:30:11 +0800

    Update 各种代理方式.py

--- 各种代理方式.py
+++ 各种代理方式.py
@@ -23,7 +23,7 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36
 'cookie': 'SID=6wULX-9_Px5BrfSFbDEVF8IGn5o7xPh9suzEZKj-JFT-Gh5eyzZPGF-Svq5ONBWiUGbSIA.; HSID=AV57W_rBXtNaFKDO5; SSID=A1VBylJpCZWZ9unzP; APISID=5vUae9w6sHvhzfF6/A5Y33cFRK3q71El6B; SAPISID=YnkIHFAjoGxIy-m-/Ams1wFv4CR7AuwPdL; OTZ=4325925_24_24__24_; NID=126=kTxZWfBUR2rqam84pCqF4MpA0VESc-DhZNLu5bLK8-NWh5BDzXvDhU99MUvBfG6ukHmiNpph3pPk_oofg2-hxMll7xBkxjidrQR4yqda_mcNyZutTWPw81zV47UGGH910Od6AOh1RBuOTvOiRFBVe3JExF6juXrjIGvZzH5ZvkSD1TVOtiYtGoENJKFAATmuU9SaeDnX-r-OdIgnIGP80-6XZaLJPcPRBBEZh786ai6NVTrFQxnx4MZrl-C2MbYtNrQAH6-NTPtNy4wNy4wBmrMTBlMtJEZsPtOdqZp-7lmZB1vQDt1048ZxAC8tMAWL5Ni7R8FF3kekgrDQUOgzynYYzFhPFzRPG_5Y97ly4FMM8Tx_F1E; 1P_JAR=2018-3-24-2; OGPC=845686784-20:; SIDCC=AAiTGe9BwWFszu8yPmULTfXtduI5T28hNwwUuhQPj0phKszX9TEy_a_Cklrw4IMRt6L0irxrPg8'}
 
 def f_by_requests():
-	proxies_socks5 = {'http':'socks5://192.168.62.201:1090','https':'socks5://192.168.62.201:1090'} 
+	proxies_socks5 = {'http':'socks5://ss的代理地址','https':'socks5://ss的代理地址'} 
 
 
 	ssl._create_default_https_context = ssl._create_unverified_context
